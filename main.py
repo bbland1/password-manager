01b0d2edcf3e6603ee35a7bb9b4647f6ecfc4848
bbland1 <104288486+bbland1@users.noreply.github.com>
2022-10-14 01:33:35 -0400

    add password generator and show/hide password

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
-from tkinter import END, Button, Canvas, Entry, Label, Tk, PhotoImage
+from tkinter import END, BooleanVar, Button, Canvas, Checkbutton, Entry, Label, Tk, PhotoImage, messagebox
+from random import choice, randint, shuffle
+import pyperclip
+
+LETTERS = "a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z".split()
+NUMBERS = "0 1 2 3 4 5 6 7 8 9".split()
+SYMBOLS = "! ? @ # : & * % $ ^".split()
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def password_generation():
+    password_entry.delete(0, END)
+
+    password_letters = [choice(LETTERS) for _ in range(randint(8,10))]
+    password_numbers = [choice(NUMBERS) for _ in range(randint(8,10))]
+    password_symbols = [choice(SYMBOLS) for _ in range(randint(8,10))]
+
+    password_values = password_letters + password_numbers + password_symbols
+    
+    shuffle(password_values)
+
+    generated_password = "".join(password_values)
+
+    password_entry.insert(0, generated_password)
+    pyperclip.copy(generated_password)
+
+# ---------------------------- HIDE AND SHOW PASSWORD FIELD ------------------------------- #
+def view_password():
+    if show_password_value.get():
+        password_entry.config(show="*")
+    else:
+        password_entry.config(show="")
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_info():
@@ -7,11 +36,20 @@ def save_info():
     name = name_entry.get()
     password = password_entry.get()
 
-    with open("data.txt", "a") as saved_passwords:
-        saved_passwords.write(f"{website} | {name} | {password}\n")
+    if website.strip() == "" or password == " ":
+        messagebox.showinfo(message="Whoops!", detail="Don't leave any fields empty!")
+    
+    else:
 
-        web_entry.delete(0, END)
-        password_entry.delete(0, END)
+        complete_save = messagebox.askokcancel(message=website, detail=f"Info to be saved: \nEmail/Username: {name} \nPassword: {password}")
+
+        if complete_save:
+            with open("data.txt", "a") as saved_passwords:
+                saved_passwords.write(f"{website} | {name} | {password}\n")
+
+                web_entry.delete(0, END)
+                password_entry.delete(0, END)
+    
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -36,12 +74,11 @@ password_label.grid(column=0, row=3)
 
 # entries
 web_entry = Entry(width=38)
+
 name_entry = Entry(width=38)
-password_entry = Entry(width=21, show="*")
+name_entry.insert(END, "testing@email.com")
 
-name_entry.insert(END, string="testing@email.com")
-
-
+password_entry = Entry(width=30, show="*")
 
 web_entry.grid(column=1, row=1, columnspan=2)
 name_entry.grid(column=1, row=2, columnspan=2)
@@ -50,12 +87,16 @@ password_entry.grid(column=1, row=3)
 web_entry.focus()
 
 # buttons
-password_button_gen = Button(text="Generate Password")
+password_button_gen = Button(text="Generate Password",width=36, command=password_generation)
 password_button_add = Button(text="Add", width=36, command=save_info)
 
+show_password_value = BooleanVar(value=True)
+show_password = Checkbutton(text="Show", onvalue=False, offvalue=True, command=view_password, variable=show_password_value)
 
-password_button_gen.grid(column=2, row=3)
-password_button_add.grid(column=1, row=4, columnspan=2)
+
+password_button_gen.grid(column=1, row=4, columnspan=2)
+password_button_add.grid(column=1, row=5, columnspan=2)
+show_password.grid(column=2, row=3)
 
 
 window.mainloop()
