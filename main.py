425035b3d5d6edc5ffc161305eb4f5ead744dc2b
bbland1 <104288486+bbland1@users.noreply.github.com>
2022-10-13 21:42:28 -0400

    adding the ability to write to a text file

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from tkinter import Button, Canvas, Entry, Label, Tk, PhotoImage
+from tkinter import END, Button, Canvas, Entry, Label, Tk, PhotoImage
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-with open("passwords.txt", mode="w") as saved_passwords:
-    saved_passwords.write()
+def save_info():
+    website = web_entry.get()
+    name = name_entry.get()
+    password = password_entry.get()
+
+    with open("data.txt", "a") as saved_passwords:
+        saved_passwords.write(f"{website} | {name} | {password}\n")
+
+        web_entry.delete(0, END)
+        password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -20,6 +28,8 @@ canvas.grid(column=1, row=0)
 web_label = Label(text="Website:")
 name_label = Label(text="Email/Username:")
 password_label = Label(text="Password:")
+
+
 web_label.grid(column=0, row=1)
 name_label.grid(column=0, row=2)
 password_label.grid(column=0, row=3)
@@ -27,7 +37,12 @@ password_label.grid(column=0, row=3)
 # entries
 web_entry = Entry(width=38)
 name_entry = Entry(width=38)
-password_entry = Entry(width=21)
+password_entry = Entry(width=21, show="*")
+
+name_entry.insert(END, string="testing@email.com")
+
+
+
 web_entry.grid(column=1, row=1, columnspan=2)
 name_entry.grid(column=1, row=2, columnspan=2)
 password_entry.grid(column=1, row=3)
@@ -36,7 +51,9 @@ web_entry.focus()
 
 # buttons
 password_button_gen = Button(text="Generate Password")
-password_button_add = Button(text="Add", width=36)
+password_button_add = Button(text="Add", width=36, command=save_info)
+
+
 password_button_gen.grid(column=2, row=3)
 password_button_add.grid(column=1, row=4, columnspan=2)
 
